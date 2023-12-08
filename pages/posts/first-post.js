e173b96253fb7339fc4c2703aa72ac7e27be4ac1
aryan bramhane <aryanbramhane1@gmail.com>
2023-12-08 15:55:43 +0530

    feat: done something

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,21 @@
-import Link from "next/link"
-import Head from "next/head"
-import Script from "next/script"
-import Layout from "../../components/Layout"
+import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/Layout";
 
-export default function FirstBlog(){
-    return(
-        <>
-            <Layout>
-
-                <Head>
-                    <title>First Post</title>
-                </Head>
-                <h1>First Post</h1>
-                <h2><Link href='/'>Back to Home</Link></h2>
-                
-            </Layout>
-        </>
-    )
+export default function FirstBlog() {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">Back to Home</Link>
+        </h2>
+        {/* new thing  */}
+      </Layout>
+    </>
+  );
 }
